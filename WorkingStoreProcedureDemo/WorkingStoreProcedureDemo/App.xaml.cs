8595992b7d4da24990fbd3040f55f51db6116fa5
tri le minh <trilm208@gmail.com>
2017-09-13 23:09:57 +0700

    #1

--- WorkingStoreProcedureDemo/WorkingStoreProcedureDemo/App.xaml.cs
+++ WorkingStoreProcedureDemo/WorkingStoreProcedureDemo/App.xaml.cs
@@ -12,6 +12,7 @@ namespace WorkingStoreProcedureDemo
         {
             InitializeComponent();
 
+
             NavigationService.NavigateAsync("NavigationPage/MainPage?title=Hello%20from%20Xamarin.Forms");
         }
 
